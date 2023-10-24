c6c495c7420200ff168b798cd59b5f73c4a40e8f
Hanseung-Jun <hsj070303@gmail.com>
2023-10-24 21:17:11 +0900

    first

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const patchList = document.getElementById("patchList");
+  const patchNote = document.getElementById("patchNote");
+
+  // 서버에서 패치 노트 파일 목록을 가져올 수도 있지만, 여기서는 간단하게 하드 코딩합니다.
+  const patchFiles = ["0.8.2.3", "0.8.2.2", "0.8.2.1"];
+
+  patchFiles.forEach((file) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = file;
+    listItem.addEventListener("click", () => {
+      fetchPatchNote(file);
+    });
+    patchList.appendChild(listItem);
+  });
+
+  if (patchFiles.length > 0) {
+    fetchPatchNote(patchFiles[0]);
+  }
+
+  function fetchPatchNote(filename) {
+    const mdBlockElement = document.createElement("md-block");
+    mdBlockElement.setAttribute("src", `patches/${filename}.md`);
+    mdBlockElement.textContent = "Loading";
+    while (patchNote.firstChild) {
+      patchNote.removeChild(patchNote.firstChild);
+    }
+    patchNote.appendChild(mdBlockElement)
+  }
+});
