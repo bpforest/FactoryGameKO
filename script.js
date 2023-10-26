c60695999dbae3d9eebfd49ee71d6294ec744615
Hanseung-Jun <hsj070303@gmail.com>
2023-10-27 01:55:28 +0900

    add 0.8.0.0
    패치 목록 분리

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
+import patchFiles from './assets/patchlists.js';
+
 document.addEventListener("DOMContentLoaded", function () {
 	const patchList = document.getElementById("patchList");
 	const patchNote = document.getElementById("patchfield");
-	const patchFiles = {
-		"업데이트 8 (실험적)": ["0.8.2.4", "0.8.2.3", "0.8.2.2", "0.8.2.1", "0.8.2.0"]
-	}
 
 	function patchToList() {
 		for (const version in patchFiles) {
@@ -29,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	function loadPatch(filename) {
 		const mdBlockElement = document.createElement("md-block");
-		mdBlockElement.setAttribute("src", `patches/${filename}.md`);
+		mdBlockElement.setAttribute("src", `./assets/patches/${filename}.md`);
 		mdBlockElement.textContent = "불러오는 중...";
 		while (patchNote.firstChild) {
 			patchNote.removeChild(patchNote.firstChild);
