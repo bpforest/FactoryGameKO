286f43d1fbe155a95145ec964862a45fd462e6c6
Hanseung-Jun <hsj070303@gmail.com>
2023-10-26 22:51:24 +0900

    UI update
    버전에 업데이트 확인 드롭다운 추가, 모바일에서 사이드바 아래로 옮김

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const patchList = document.getElementById("patchList");
-  const patchNote = document.getElementById("patchNote");
+	const patchList = document.getElementById("patchList");
+	const patchNote = document.getElementById("patchfield");
+	const patchFiles = {
+		"업데이트 8 (실험적)": ["0.8.2.4", "0.8.2.3", "0.8.2.2", "0.8.2.1", "0.8.2.0"]
+	}
 
-  // 서버에서 패치 노트 파일 목록을 가져올 수도 있지만, 여기서는 간단하게 하드 코딩합니다.
-  const patchFiles = ["0.8.2.4", "0.8.2.3", "0.8.2.2", "0.8.2.1", "0.8.2.0"];
+	function patchToList() {
+		for (const version in patchFiles) {
+			const listVersion = document.createElement("details");
+			if (version === Object.keys(patchFiles)[0]) {
+				listVersion.setAttribute("open", "");
+			}
+			const listSum = document.createElement("summary");
+			listSum.textContent = version;
+			listVersion.appendChild(listSum);
+			const listPatch = document.createElement("ul");
+			listVersion.appendChild(listPatch);
+			for (const patch in patchFiles[version]) {
+				const patchVer = patchFiles[version][patch];
+				const valuePatch = document.createElement("li");
+				valuePatch.textContent= patchVer;
+				valuePatch.addEventListener("click", () => loadPatch(patchVer));
+				listPatch.appendChild(valuePatch);
+			}
+			patchList.appendChild(listVersion);
+		}
+	}
 
-  patchFiles.forEach((file) => {
-    const listItem = document.createElement("li");
-    listItem.textContent = file;
-    listItem.addEventListener("click", () => {
-      fetchPatchNote(file);
-    });
-    patchList.appendChild(listItem);
-  });
+	function loadPatch(filename) {
+		const mdBlockElement = document.createElement("md-block");
+		mdBlockElement.setAttribute("src", `patches/${filename}.md`);
+		mdBlockElement.textContent = "불러오는 중...";
+		while (patchNote.firstChild) {
+			patchNote.removeChild(patchNote.firstChild);
+		}
+		patchNote.appendChild(mdBlockElement);
+	}
 
-  if (patchFiles.length > 0) {
-    fetchPatchNote(patchFiles[0]);
-  }
-
-  function fetchPatchNote(filename) {
-    const mdBlockElement = document.createElement("md-block");
-    mdBlockElement.setAttribute("src", `patches/${filename}.md`);
-    mdBlockElement.textContent = "Loading";
-    while (patchNote.firstChild) {
-      patchNote.removeChild(patchNote.firstChild);
-    }
-    patchNote.appendChild(mdBlockElement)
-  }
+	if (0 < Object.keys(patchFiles).length) {
+		patchToList()
+		loadPatch(patchFiles[Object.keys(patchFiles)[0]][0])
+	}
 });
