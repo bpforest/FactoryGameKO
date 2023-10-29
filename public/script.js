eac6904bc0788aaa2f4630a4f0f9c51c913794eb
Hanseung-Jun <hsj070303@gmail.com>
2023-10-30 02:04:40 +0900

    UI update

--- public/script.js
+++ public/script.js
@@ -1,35 +1,35 @@
 import patchFiles from './assets/patchlists.js';
 
 document.addEventListener("DOMContentLoaded", () => {
-	const patchList = document.getElementById("patchList")
-	const patchNote = document.getElementById("patchfield")
+	const patchListField = document.getElementById("patchList")
+	const patchNoteField = document.getElementById("patchfield")
 
 	// 버전을 html 리스트로 변환
 	function patchToList() {
-		for (const version in patchFiles) {
-			const listVersion = document.createElement("details")
+		for (const release in patchFiles) {
+			const listVersion = document.createElement("ul")
+			listVersion.setAttribute("class", "release")
+			listVersion.innerHTML = `
+			<div class="category">${release}</div>`
 
-			// 상단 버전일 경우 열어놓기
-			if (version === Object.keys(patchFiles)[0]) {
-				listVersion.setAttribute("open", "")
-			}
-			const listSum = document.createElement("summary")
-			listSum.textContent = version
-			listVersion.appendChild(listSum)
-
-			const listPatch = document.createElement("ul")
-			listVersion.appendChild(listPatch)
-			for (const patch in patchFiles[version]) {
+			const versionList = document.createElement("ul")
+			for (const patch in patchFiles[release]) {
+				const patchVer = patchFiles[release][patch]
 				const valuePatch = document.createElement("li")
-				const patchVer = patchFiles[version][patch]
-				valuePatch.textContent= patchVer
+				valuePatch.setAttribute("id", patchVer)
+				valuePatch.innerHTML = `
+				<svg fill="none" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+					<path d="M8.75 11.5C8.33579 11.5 8 11.8358 8 12.25C8 12.6642 8.33579 13 8.75 13H15.25C15.6642 13 16 12.6642 16 12.25C16 11.8358 15.6642 11.5 15.25 11.5H8.75ZM8.75 14.25C8.33579 14.25 8 14.5858 8 15C8 15.4142 8.33579 15.75 8.75 15.75H15.25C15.6642 15.75 16 15.4142 16 15C16 14.5858 15.6642 14.25 15.25 14.25H8.75ZM8.75 17C8.33579 17 8 17.3358 8 17.75C8 18.1642 8.33579 18.5 8.75 18.5H15.25C15.6642 18.5 16 18.1642 16 17.75C16 17.3358 15.6642 17 15.25 17H8.75ZM13.585 2.586L19.414 8.414C19.789 8.789 20 9.298 20 9.828V20C20 21.104 19.104 22 18 22H6C4.896 22 4 21.104 4 20V4C4 2.896 4.896 2 6 2H12.172C12.1999 2 12.2271 2.00371 12.2542 2.00741C12.2738 2.01008 12.2933 2.01274 12.313 2.014C12.528 2.029 12.74 2.07 12.937 2.152C12.9944 2.17648 13.0488 2.20797 13.103 2.23933C13.1197 2.24897 13.1363 2.25859 13.153 2.268C13.1685 2.27647 13.1845 2.28426 13.2005 2.29207C13.2281 2.30548 13.2557 2.31894 13.281 2.336C13.359 2.389 13.429 2.452 13.5 2.516C13.5115 2.5262 13.5238 2.53567 13.5363 2.5452C13.5531 2.55808 13.57 2.57105 13.585 2.586ZM18 20.5C18.276 20.5 18.5 20.275 18.5 20V10H14C12.896 10 12 9.104 12 8V3.5H6C5.724 3.5 5.5 3.725 5.5 4V20C5.5 20.275 5.724 20.5 6 20.5H18ZM17.378 8.5L13.5 4.621V8C13.5 8.275 13.724 8.5 14 8.5H17.378Z"/>
+				</svg>
+				<div class="version">${patchVer}</div>`
 				valuePatch.addEventListener("click", (event) => {
 					event.preventDefault()
 					window.location.href = patchVer
 				})
-				listPatch.appendChild(valuePatch)
+				versionList.appendChild(valuePatch)
 			}
-			patchList.appendChild(listVersion)
+			listVersion.appendChild(versionList)
+			patchListField.appendChild(listVersion)
 		}
 	}
 
@@ -42,11 +42,11 @@ document.addEventListener("DOMContentLoaded", () => {
 		const disqusElement = document.createElement("div");
 		disqusElement.setAttribute("id", "disqus_thread");
 
-		while (patchNote.firstChild) {
-			patchNote.removeChild(patchNote.firstChild);
+		while (patchNoteField.firstChild) {
+			patchNoteField.removeChild(patchNoteField.firstChild);
 		}
-		patchNote.appendChild(mdBlockElement);
-		patchNote.appendChild(disqusElement);
+		patchNoteField.appendChild(mdBlockElement);
+		patchNoteField.appendChild(disqusElement);
 		(function() {
 			var d = document, s = d.createElement('script');
 			s.src = 'https://factorygameko.disqus.com/embed.js';
@@ -59,5 +59,7 @@ document.addEventListener("DOMContentLoaded", () => {
 	patchToList()
 	if (window.location.pathname != "/") {
 		loadPatch()
+		const versionElement = document.getElementById(window.location.pathname.substring(1))
+		versionElement.setAttribute("class", "selected")
 	}
 });
